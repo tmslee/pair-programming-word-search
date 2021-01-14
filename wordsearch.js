0894a9aeed857505f3aa20b0d917461b8cc18805
Sang Hyup Lee <tms.lee9@gmail.com>
2021-01-14 00:23:38 +0000

    re-wrote wordsearch.js

--- wordsearch.js
+++ wordsearch.js
@@ -1,22 +1,35 @@
 const wordSearch = (letters, word) => { 
     if(letters.length === 0 || letters[0].legnth === 0) return false;
-    let rows = letters.length;
-    let cols = letters[0].length;
+    if(word.lengh === 0) return true;
 
-    for (let i = 0; i < letters.length; i++) {
-        if(letters[i].join('').includes(word)) return true 
+    for(let r = 0; r < letters.length; r ++){
+        for(let c = 0; c < letters[0].length; c++){
+            if(hSearch(r, c, word, letters) || vSearch(r, c, word, letters)) return true;
+        }
     }
     
-    for(let col = 0 ; col < cols ; col ++){
-        let colStr = '';
-        for(let row = 0 ; row < rows ;row ++){
-            colStr += letters[row][col];
-        }
-        if(colStr.includes(word)) return true;
-    }
-
     return false;
 }                                                                                                                       
+
+const hSearch = (r, c, word, letters) => {
+    if(letters[0].length - c >= word.length){
+        for(let idx = 0; idx<word.length ; idx++){
+            if(letters[r][c+idx] !== word[idx]) return false;
+        }
+        return true;
+    }
+    return false;
+}
+
+const vSearch = (r, c, word, letters) => {
+    if(letters.length - r >= word.length){
+        for(let idx = 0; idx<word.length ; idx++){
+            if(letters[r+idx][c] !== word[idx]) return false;
+        }
+        return true;
+    }
+    return false;
+}
 
 module.exports = wordSearch
 
